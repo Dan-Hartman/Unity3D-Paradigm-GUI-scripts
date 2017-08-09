7f1de43600e629282d575d50f6307476682427c5
Dan Hartman <daniel.hartman4761@gmail.com>
2017-08-08 20:41:29 -0400

    no message

--- Scripts/Pod_Telemetry.js
+++ Scripts/Pod_Telemetry.js
@@ -2,7 +2,7 @@
 // Get the latest pod telemetry data from localhost:7777/state
 public var url: String = "http://localhost:7777/state";
 public var json: String;
-public var time: int;
+public var velocity: int;
 
 function Start () {
 	var wwwInitial: WWW = new WWW(url);
@@ -10,19 +10,8 @@ function Start () {
     if (wwwInitial.isDone) {
     	json = wwwInitial.text;
     	Debug.Log("Data retrieved: " + json);
-    	InvokeRepeating("updateTime", 0.01, 2.0);
+	   	InvokeRepeating("updateJson", 0.01, 2.0);
     }
-//    renderer.material.mainTexture = www.texture;
-}
-
-function updateTime() {
-	time+=2;
-}
-
-function Update () {
-	if(time > 0 && time % 2 == 0) {
-		StartCoroutine("updateJson");
-	}
 }
 
 function updateJson() {
@@ -30,5 +19,13 @@ function updateJson() {
 	if (wwwUpdated.isDone) {
 		json = wwwUpdated.text;
 		Debug.Log("Data updated to: " + json);
+	}
+}
+
+function Update () {
+	for (var child : Transform in transform) {
+		if (child.position.z > 90) {
+	    child.position -= Vector3.forward * velocity / 140;
+	    }
 	}
 }
