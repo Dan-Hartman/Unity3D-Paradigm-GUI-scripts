9a3e37a484f8d907132fe583ae4795a72ce14e49
Dan Hartman <daniel.hartman4761@gmail.com>
2017-08-23 01:18:44 -0700

    Implemented a 3rd camera

--- Scripts/camera_swap.js
+++ Scripts/camera_swap.js
@@ -1,4 +1,50 @@
 #pragma strict
+
+// Three cameras
+var firstPersonCamera: Camera;
+var overheadCamera: Camera;
+var frontCamera: Camera;
+
+// Used to initialize.
+function Start() {
+	firstPersonCamera.enabled = false;
+	frontCamera.enabled = false;
+	overheadCamera.enabled = true;
+}
+
+// Called every frame.
+function Update() {
+	if (Input.GetKeyDown("1")) {
+        ShowFirstPersonView();
+    }
+    if (Input.GetKeyDown("2")) {
+        ShowOverheadView();
+    }
+    if (Input.GetKeyDown("3")) {
+    	ShowFrontView();
+    }
+}
+
+// Swap to the overhead pov
+function ShowOverheadView() {
+    firstPersonCamera.enabled = false;
+    frontCamera.enabled = false;
+    overheadCamera.enabled = true;
+}
+
+// Swap to the first person pov
+function ShowFirstPersonView() {
+    overheadCamera.enabled = false;
+    frontCamera.enabled = false;
+    firstPersonCamera.enabled = true;
+}
+
+// Swap to the front pod view
+function ShowFrontView() {
+	firstPersonCamera.enabled = false;
+	overheadCamera.enabled = false; 
+	frontCamera.enabled = true;
+}#pragma strict
 // Two cameras
 var firstPersonCamera: Camera;
 var overheadCamera: Camera;
