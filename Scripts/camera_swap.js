a69199b5849fcc68ff2d9724cb1c89cbbc53175b
Dan Hartman <daniel.hartman4761@gmail.com>
2017-08-24 15:36:28 -0700

    Update camera_swap.js

--- Scripts/camera_swap.js
+++ Scripts/camera_swap.js
@@ -44,33 +44,4 @@ function ShowFrontView() {
 	firstPersonCamera.enabled = false;
 	overheadCamera.enabled = false; 
 	frontCamera.enabled = true;
-}#pragma strict
-// Two cameras
-var firstPersonCamera: Camera;
-var overheadCamera: Camera;
-
-function Start() {
-	firstPersonCamera.enabled = false;
-	overheadCamera.enabled = true;
-}
-
-function Update() {
-	if (Input.GetKeyDown("1")) {
-        ShowFirstPersonView();
-    }
-    if (Input.GetKeyDown("2")) {
-        ShowOverheadView();
-    }
-}
-
-// Swap to the overhead pov
-function ShowOverheadView() {
-    firstPersonCamera.enabled = false;
-    overheadCamera.enabled = true;
-}
-
-// Swap to the first person pov
-function ShowFirstPersonView() {
-    firstPersonCamera.enabled = true;
-    overheadCamera.enabled = false;
 }
